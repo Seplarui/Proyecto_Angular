b789607fff14b0762463bcf4bc3714b400b5407d
Sebas <seplarui@gmail.com>
2017-08-29 12:45:20 +0200

    Restaurante List Component

---
+++ app/components/restaurantes-lists.component.ts
@@ -0,0 +1,18 @@
+import { Component } from "angular2/core";
+import { ROUTER_DIRECTIVES, RouteConfig, Router } from "angular2/router";
+
+@Component({
+
+    selector: "restaurantes-list",
+    templateUrl: "app/view/restaurantes-list.html",
+    
+})
+
+export class RestaurantesListComponent {
+    //PROPIEDADES
+
+    public titulo: string = "Restaurantes";
+
+
+
+}
