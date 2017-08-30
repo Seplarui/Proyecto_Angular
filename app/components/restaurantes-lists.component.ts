2d02d1de8c9dc109e93ac5b9da03152a55da0f79
Sebas Sebas <seplarui@gmail.com>
2017-08-30 10:26:40 +0200

    Crear servicio, http

--- app/components/restaurantes-lists.component.ts
+++ app/components/restaurantes-lists.component.ts
@@ -1,17 +1,28 @@
 import { Component } from "angular2/core";
 import { ROUTER_DIRECTIVES, RouteConfig, Router } from "angular2/router";
+import { RestauranteService } from "../services/restaurante.service";
 
 @Component({
 
     selector: "restaurantes-list",
     templateUrl: "app/view/restaurantes-list.html",
-    
+    providers: [RestauranteService]
+
 })
 
 export class RestaurantesListComponent {
     //PROPIEDADES
 
     public titulo: string = "Listado de Restaurantes";
+
+    constructor(private _restauranteService: RestauranteService) { }
+
+
+    ngOninit() {
+        console.log("restaurantes-list component cargado");
+
+    }
+
 
 
 
